416ac4457463746b8e772fb463a044424a2920c4
Alamin4D <alaminahmed79000@gmail.com>
2026-09-01 22:41:46 +0600

    chore: initialize backend project

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+import httpStatus from "http-status"
+import { globalErrorHandler } from './app/middleware/globalErrorHandler'
+import config from './app/config'
+import { notFound } from './app/middleware/notFound'
+
+const app: Application = express()
+
+app.use(
+    cors({
+        origin: config.frontend_url,
+        credentials: true,
+    }),
+)
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }))
+
+// Middleware to parse JSON bodies
+app.use(express.json())
+app.use(cookieParser())
+
+// app.use('/api/v1/auth', AuthRoutes)
+
+// Basic route
+app.get('/', async (req: Request, res: Response) => {
+    res.status(httpStatus.OK).json({
+        success: true,
+        message: 'Welcome to FieldPro Backend',
+    })
+})
+
+app.use(globalErrorHandler)
+app.use(notFound)
+
+export default app
