ae0f7c7b5b6fcb5e0278e830d049fc2a97843365
Alamin4D <alaminahmed79000@gmail.com>
2026-09-03 13:03:02 +0600

    feat: add service validation with zod

--- src/app/module/service/service.validation.ts
+++ src/app/module/service/service.validation.ts
@@ -34,6 +34,7 @@ export const updateServiceSchema = createServiceSchema.partial().extend({
 
 export const serviceQuerySchema = z.object({
 	search: z.string().optional(),
+    
 	category: z.string().optional(),
 
 	isActive: z.enum(["true", "false"]).optional(),
